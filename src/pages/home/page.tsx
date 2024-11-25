f966c2e06129cabc4fe5d942eea5bd10a350c82d
Amr Elskhawy <amrelskhawy@gmail.com>
2024-11-25 23:06:07 +0200

    Add section Row to the Home Page

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,3 +1,4 @@
+import { SectionRow } from "../../components/SectionRow";
 import Hero from "./components/Hero";
 
 export interface HomeProps {
@@ -6,8 +7,9 @@ export interface HomeProps {
 
 export function Home({}: HomeProps) {
   return (
-    <>
+    <div className="grid gap-8">
       <Hero />
-    </>
+      <SectionRow tag={""} title={""} products={[]} />
+    </div>
   );
 }
