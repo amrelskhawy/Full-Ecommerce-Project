7dad191c37084d6071d4fd0311e50600084c5c24
Amr Elskhawy <amrelskhawy@gmail.com>
2024-11-25 22:24:46 +0200

    re import the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import { Home } from "./pages/home/page"
-import { Navbar } from "./components/Navbar/Navbar"
+import { Navbar } from "./components/Navbar"
 
 
 function App() {
